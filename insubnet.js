3cb54ab5f802ef58446a726abdd38a2172f44737
Louis T. <louist@ltdev.im>
2013-11-06 15:02:04 -0800

    Update insubnet.js

--- insubnet.js
+++ insubnet.js
@@ -4,12 +4,12 @@
 */
 Exporter(function (isNode) {
    var proto = {
-       version: '0.0.2',
-       Auto: function (ip, subnet, mask) {
+       version: '0.0.3',
+       Auto: function (ip, subnet, prefix) {
              if (!ip || !subnet) {
                 return false;
              };
-             var sm = this.getMask(subnet,mask);
+             var sm = this.getPrefix(subnet,prefix);
              if (sm[0] && sm[1] && !isNaN(sm[1])) {
                 if (this.isIPv4(ip)) {
                    return this.__IPv4(ip,sm[0],sm[1]);
@@ -19,28 +19,28 @@ Exporter(function (isNode) {
              };
              return false;
        },
-       IPv4: function (ip, subnet, mask) {
-             var sm = this.getMask(subnet,mask);
+       IPv4: function (ip, subnet, prefix) {
+             var sm = this.getPrefix(subnet,prefix);
              return this.__IPv4(ip,sm[0],sm[1]);
        },
-       __IPv4: function (ip, subnet, mask) {
-             if (this.isIPv4(ip) && this.isIPv4(subnet) && (mask && !isNaN(mask))) {
-                return ((this.toInt(ip)&-1<<(32-mask)) == this.toInt(subnet));
+       __IPv4: function (ip, subnet, prefix) {
+             if (this.isIPv4(ip) && this.isIPv4(subnet) && (prefix && !isNaN(prefix))) {
+                return ((this.toInt(ip)&-1<<(32-prefix)) == this.toInt(subnet));
              };
              return false;
        },
        isIPv4: function (ip) {
              return /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$/.test(ip);
        },
-       IPv6: function (ip, subnet, mask) {
-             var sm = this.getMask(subnet,mask);
+       IPv6: function (ip, subnet, prefix) {
+             var sm = this.getPrefix(subnet,prefix);
              return this.__IPv6(ip,sm[0],sm[1]);
        },
-       __IPv6: function (ip, subnet, mask) {
-             if (this.isIPv6(ip) && this.isIPv6(subnet) && (mask && !isNaN(mask))) {
-                var o = Array.apply(null,Array(Math.floor(mask/16))).map(function(_){ return 0xffff; });
-                if (mask%16) {
-                   o.push((1<<mask%16)-1);
+       __IPv6: function (ip, subnet, prefix) {
+             if (this.isIPv6(ip) && this.isIPv6(subnet) && (prefix && !isNaN(prefix))) {
+                var o = Array.apply(null,Array(Math.floor(prefix/16))).map(function(_){ return 0xffff; });
+                if (prefix%16) {
+                   o.push((1<<prefix%16)-1);
                 };
                 var mask = o.concat(Array.apply(null,Array(8-o.length)).map(function(_){ return 0; }));     
                 ip = this.toInt(ip,6), subnet = this.toInt(subnet,6);
@@ -49,7 +49,7 @@ Exporter(function (isNode) {
              return false;
        },
        isIPv6: function (ip) {
-             return !!(this.expand(ip));
+             return !!(this.Expand(ip));
        },
        __isIPv6: function (ip) {
              return /^([0-9a-f]{4}|0)(\:([0-9a-f]{4}|0)){7}$/i.test(ip);
@@ -57,26 +57,54 @@ Exporter(function (isNode) {
        isIP: function (ip) {
              return (this.isIPv4(ip)||this.isIPv6(ip));
        },
-       getMask: function (subnet, mask) {
-             if (subnet && subnet.indexOf('/') != -1) {
+       Validate: function (ip) {
+              if (this.subnets && this.isIP(ip) && (!!(this.subnets.ipv4.length)||!!(this.subnets.ipv6.length))) {
+                 var subs = this.subnets[(this.isIPv4(ip)?"ipv4":"ipv6")];
+                 for (var num in subs) {
+                     if (this.Auto(ip,subs[num])) {
+                        return true;
+                     };
+                 };
+              };
+              return false;
+       },
+       setSubnets: function (subnets) {
+             this.subnets = {ipv4:[],ipv6:[]};
+             if (Array.isArray(subnets)) {
+                for (var num in subnets) {
+                    var subnet = subnets[num];
+                    if (/\/(\d+){1,3}$/.test(subnet)) {
+                       if (this.isIPv4(subnet.split('/')[0])) {
+                          this.subnets.ipv4.push(subnet);
+                        } else if (this.isIPv6(subnet.split('/')[0])) {
+                          this.subnets.ipv6.push(subnet);   
+                       };
+                    };
+                };
+                return (!!(this.subnets.ipv4.length)||!!(this.subnets.ipv6.length));
+             };
+             return false;
+       },
+       getPrefix: function (subnet, prefix) {
+             if (/\/(\d+){1,3}$/.test(subnet)) {
                 try {
                    var split = subnet.split('/');
                        subnet = split[0],
-                       mask = split[1];
+                       prefix = split[1];
                 } catch (e) { }; // Handle the error!?
              };
-             return [subnet,mask];
+             return [subnet,prefix];
        },
        toInt: function (ip,mode) {
              switch (mode) {
                     case 6:
-                         return this.expand(ip).split(":").map(function(x){return parseInt(x,16)});
+                         return this.Expand(ip).split(":").map(function(x){return parseInt(x,16)});
                     case 4:
                     default:
                          return ((ip=ip.split('.'))[0]<<24|ip[1]<<16|ip[2]<<8|ip[3]);
              };
        },
-       expand: function (ip) {
+       Expand: function (ip) {
              var ip = String(ip);
              if (ip.indexOf("::") != -1) {
                 if (ip.match(/::/g).length > 1) {
